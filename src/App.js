7af746a9ffe8ea84927b6310b30a2575b6310892
Tanya Robertson <tanya.robertson@claytonhomes.com>
2019-05-08 13:45:38 -0400

    console.log

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import NewRecipe from './components/Forms/NewRecipe';
 
 class App extends Component {
     render() {
+
+    console.log("this is the env...." + process.env.PUBLIC_URL);
+
         return (
             <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div className="app">
