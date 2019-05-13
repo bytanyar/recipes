ecf3a2855742f79acbed1bf13457a02804c5fecd
Tanya Robertson <tanya.robertson@claytonhomes.com>
2019-05-13 15:35:29 -0400

    TRR-007    styling for differing sizes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import AlfredoImage from '../images/alfredo-sauce.jpg';
 import AmarettoImage from '../images/amaretto.jpeg';
 import ChickenImage from '../images/amazing-chicken.jpg';
+import TableTopImage from '../images/tabletop.jpg';
 
 import { HomeStyles } from './home.styled';
 
@@ -16,21 +17,28 @@ class Home extends Component {
     }
 
     componentDidMount() {
-        window.addEventListener('scroll', this._onScroll);
+        window.addEventListener('scroll', this._pastaOnScroll);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('scroll', this._onScroll);
+        window.removeEventListener('scroll', this._pastaOnScroll);
     }
 
-    _onScroll(){
+    _pastaOnScroll(){
         const sauce = document.getElementById('sauce');
 
         const yPos = 0 - window.pageYOffset/1.2;
         sauce.style.top = 200 + yPos + "px";
     }
+    _drinkOnScroll(){
+        const drink = document.getElementById('drink');
+
+        const yPos = 0 - window.pageYOffset/5;
+        drink.style.top = 1250 + yPos + "px";
+    }
 
     render() {
+        const bgStyle = { backgroundImage: `url(${TableTopImage})`};
         return (
             <HomeStyles className="home">
                 <div id="chicken" className="chicken">
@@ -38,9 +46,12 @@ class Home extends Component {
                 </div>
                 <div id="sauce" className="sauce">
                     <div>
-                        <h3>What do you know?</h3>
+                        <h2>What&rsquo;s for dinner?</h2>
                     </div>
                     <img src={AlfredoImage} alt="sauce" />
+                </div>
+                <div id="tabletop" className="tabletop" style={bgStyle}>
+                    <h3>It&rsquo;s gotta be something good!</h3>
                 </div>
                 <div id="drink" className="drink">
                     <img src={AmarettoImage} alt="drink" />
--- src/components/home.styled.js
+++ src/components/home.styled.js
@@ -9,19 +9,14 @@ export const HomeStyles = styled.div`
     }
     .chicken {
         position: absolute;
-        top: 150px;
-        z-index: 1;
-    }
-    .drink {
-        position: relative;
-        top: 150vw;
+        top: 200px;
     }
     .sauce {
         position: absolute;
-        top: 200px;
+        top: 300px;
         z-index: 5;
 
-        h3 {
+        h2 {
             color: white;
             font-size: 1rem;
             margin: 5vh 15px;
@@ -29,20 +24,70 @@ export const HomeStyles = styled.div`
             text-shadow: 2px 2px 0 #333;
         }
     }
+    .tabletop {
+        background-size: 100vw;
+        height: 85vh;
+        position: relative;
+        top: 190px;
+
+        h3 {
+            color: white;
+            font-size: 1rem;
+            margin: 0 15px;
+            padding-top: 40vh;
+            text-align: center;
+            text-shadow: 2px 2px 0 #333;
+        }
+    }
+    .drink {
+        position: absolute;
+        top: 800px;
+        z-index: 5;
+    }
     @media only screen and (min-width: 400px){
+        .chicken {
+            top: 150px;
+        }
         .sauce {
-            h3 {
+            top: 200px;
+            
+            h2 {
                 font-size: 2rem;
                 margin: 10vh 12.5%;
             }
         }
+        .tabletop {
+            top: 375px;
+
+            h3 {
+                font-size: 2rem;
+                margin: 0 12.5%;
+                padding-top: 40vh;
+            }
+        }
+        .drink {
+            top: 900px;
+        }
     }
     @media only screen and (min-width: 768px){
-        .sauce {
+        .tabletop {
+            height: 150vh;
+            top: 1000px;
+
             h3 {
+                font-size: 4rem;
+                margin: 0 25%;
+                padding-top: 70vh;
+            }
+        }
+        .sauce {
+            h2 {
                 font-size: 3rem;
                 margin: 20vh 25%;
             }
+        }
+        .drink {
+            top: 2000px;
         }
     }
 `;
