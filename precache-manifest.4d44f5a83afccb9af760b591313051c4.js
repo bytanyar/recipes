6e218fe3f823a6b7356c3a17c1ee3ebfd6a362f7
bytanyar <bytanyar@yahoo.com>
2019-05-24 11:44:10 -0400

    Updates

--- precache-manifest.4d44f5a83afccb9af760b591313051c4.js
+++ precache-manifest.4d44f5a83afccb9af760b591313051c4.js
@@ -4,24 +4,24 @@ self.__precacheManifest = [
     "url": "/recipes/static/media/amaretto.fe214cc2.jpeg"
   },
   {
-    "revision": "413f01fc6c7b02c796d1",
-    "url": "/recipes/static/css/main.8e715b28.chunk.css"
+    "revision": "1f033440b42084a84202",
+    "url": "/recipes/static/css/main.9f3c6809.chunk.css"
   },
   {
     "revision": "c96e90947479d05374fe",
     "url": "/recipes/static/js/runtime~main.19e64e71.js"
   },
   {
-    "revision": "7e2a6292b0ddc95d4a41",
-    "url": "/recipes/static/js/2.daa2ef77.chunk.js"
+    "revision": "af956ba18c483050cc28",
+    "url": "/recipes/static/js/2.eb5388bd.chunk.js"
   },
   {
     "revision": "af8a7793d56f4e659eba29533eb2e840",
     "url": "/recipes/static/media/alfredo-sauce.af8a7793.jpg"
   },
   {
-    "revision": "413f01fc6c7b02c796d1",
-    "url": "/recipes/static/js/main.2c6288b3.chunk.js"
+    "revision": "1f033440b42084a84202",
+    "url": "/recipes/static/js/main.02df0015.chunk.js"
   },
   {
     "revision": "cdf18a3a98646f409f4591c3b2108293",
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "/recipes/static/media/lorem-ipsum-logo.e41085e5.png"
   },
   {
-    "revision": "62ed745e82d27c89fc04a9bbe670a62c",
-    "url": "/recipes/static/media/my_cookbook2019_05_20_12_23_50.62ed745e.xml"
+    "revision": "34b24d443a552a3ed22de74721e825b1",
+    "url": "/recipes/static/media/my_cookbook2019_05_20_12_23_50.34b24d44.xml"
   },
   {
-    "revision": "f94edaa4ca2f91db75fe446f3f611343",
+    "revision": "f4d09c5acd67a5436db152ec8b3efcc4",
     "url": "/recipes/index.html"
   }
 ];
