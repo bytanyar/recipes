7e895b26827f8bb62d3fcd58d550604d853a696e
Tanya Robertson <bytanyar@yahoo.com>
2019-08-20 15:59:18 -0400

    TRR-013 add service

---
+++ services/getData.js
@@ -0,0 +1,28 @@
+let data = null;
+
+const appDataURL = '/data.json'
+
+export const getData = () => {
+
+    return new Promise((resolve, reject) => {
+        if (data) {
+            resolve(data);
+        } else {
+            fetch(appDataURL, {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                mode: 'cors'
+            })
+                .then(response => response.json())
+                .then(json => {
+                    data = json;
+                    resolve(json);
+                })
+                .catch(err => reject(err));
+        }
+    });
+}
+export default getData;
